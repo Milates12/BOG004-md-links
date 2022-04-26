5b5fd81f676cf61bb0b596d50b99c43aee28cd29
unknown <accscortes09@gmail.com>
2022-04-26 13:40:36 -0500

    Agregando constructor de promesa para validar si la ruta es de un archivo existente o no

--- src/index.js
+++ src/index.js
@@ -2,37 +2,43 @@
 //   // ...
 // };
 
-import { access } from 'fs/promises';
-import { constants } from 'fs';
-import { relative, resolve, } from 'path';
-import { fileURLToPath } from 'url';
+const fs = require("fs");
+const path = require("path");
+
+//const path = process.argv[2]
+const rutaRelativa = '../../../MD-fuera/ejemplo.txt';
+
+const checkFileExists = (filepath) => {
+    return new Promise((res, rej) => {
+    fs.access(filepath, fs.constants.F_OK, (err) => {
+        const validation = err ? 'does not exist' : 'exists';
+        console.log(validation);
+        if (validation === 'exists'){
+            res(console.log(path.resolve(filepath)));
+        } else if (validation === 'does not exist'){
+            rej(console.log('No existe esa ruta'));
+        }
+    });
+});
+}
+
+checkFileExists(rutaRelativa)
+.then(()=>{})
+.catch(()=>{})
 
 /*Ejemplos de rutas para obtener los argumentos*/
-const rutaAbsoluta = 'C:/Users/Camila/Desktop/MD-fuera/ejemplo.md';
-const rutaRelativa = '../../../MD-fuera';
-const dontExist = '../../../MD-fuera/ejemplo.md'
+// const rutaAbsoluta = 'C:/Users/Camila/Desktop/MD-fuera/ejemplo.md';
+// const rutaRelativa = '../../../MD-fuera';
+// const dontExist = process.argv[2]
+// //'../../../MD-fuera/ejemplo.md'
 
-const siRutaExiste = access(dontExist, constants.F_OK)
-.then(() => {
-    const absolutePath = resolve(dontExist);
-    console.log(`El archivo existe y se convierte en ruta absoluta: ${absolutePath}`)
-})
-.catch(() => console.error('Please, enter a path to an existing file or directory'));
-
-// const exist = access(dontExist, constants.F_OK, (err) => {
-//     const validateExistPath = err ? 'does not exist' : 'exists';
-//     if (validateExistPath === 'does not exist'){
-//         console.log('Please, enter a path to an existing file or directory');
-//     } else if (validateExistPath === 'exists'){
-//         const absolutePath = resolve(dontExist);
-//         // return absolutePath;
-//         console.log('La ruta existe y se convierte a ruta absoluta', absolutePath);
-//     }
-// });
+// const existingPath = access(dontExist, constants.F_OK)
+// .then(() => {
+//     const absolutePath = resolve(dontExist);
+//     console.log(`El archivo existe y se convierte en ruta absoluta: ${absolutePath}`)
+// })
+// .catch(() => console.error('Please, enter a path to an existing file or directory'));
 
 
-
-
-
-
-
+    
+;
